594aca4f64c8717f4a2150ad84a153a8807e41db
soufiane <soufianeelbasraoui0@gmail.com>
2026-09-18 22:46:48 +0200

    ajout du composant PublicRoute et mise à jour des pages de connexion et d'inscription

---
+++ src/routes/PublicRoute.jsx
@@ -0,0 +1,30 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+function PublicRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    try {
+      const user = jwtDecode(token);
+
+      if (user.role === "ADMIN") {
+        return <Navigate to="/admin/dashboard" replace />;
+      }
+
+      if (user.role === "TRANSPORTEUR") {
+        return <Navigate to="/transporteur/dashboard" replace />;
+      }
+
+      if (user.role === "EXPEDITEUR") {
+        return <Navigate to="/expediteur/dashboard" replace />;
+      }
+    } catch (error) {
+      localStorage.removeItem("token");
+    }
+  }
+
+  return <Outlet/>;
+}
+
+export default PublicRoute;
